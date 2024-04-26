170440a6b5796747684c0bbc953eb9a10fe91fe3
Мария Хитринович <217097@edu.fa.ru>
2024-04-26 11:32:31 +0300

    кнопкм удаления сделаны ура

--- InternetBookShop/Clients.xaml.cs
+++ InternetBookShop/Clients.xaml.cs
@@ -22,7 +22,51 @@ namespace InternetBookShop
     {
         public Clients()
         {
+
             InitializeComponent();
+            ClientData.ItemsSource = InternetBookShop_KyrcahEntities.GetContext().Client.ToList();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new BD());
+        }
+
+        private void RedactButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToString(RedactButton.Content)=="Редактировать")
+            {
+                RedactButton.Content = "Выйти из режима редактирования";
+                ClientData.IsReadOnly = false;
+                DeleteButton.Visibility = Visibility.Visible;
+                DeleteButton.IsEnabled = true;
+
+                SaveButton.Visibility = Visibility.Visible;
+                SaveButton.IsEnabled = true;
+            }
+            else
+            {
+                RedactButton.Content= "Редактировать";
+                DeleteButton.Visibility = Visibility.Hidden;
+                DeleteButton.IsEnabled = false;
+                SaveButton.Visibility = Visibility.Hidden;
+                SaveButton.IsEnabled = false;
+                ClientData.IsReadOnly = true;
+            }
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var row = ClientData.SelectedItems.Cast<Client>().ToList();
+            InternetBookShop_KyrcahEntities.GetContext().Client.RemoveRange(row);
+            InternetBookShop_KyrcahEntities.GetContext().SaveChanges();
+            MessageBox.Show("пися");
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            InternetBookShop_KyrcahEntities.GetContext().SaveChanges();
         }
     }
 }
